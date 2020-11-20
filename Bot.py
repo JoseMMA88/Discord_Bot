9a46ee816a00b93cb09858e396807039e9240e57
JoseMMA88 <josemma_97@hotmail.com>
2020-11-20 11:34:52 +0100

    Join members event, control channels, control user nickname ADDED

--- Bot.py
+++ Bot.py
@@ -1,6 +1,8 @@
 # TO INVITE DE BOT
 #https://discordapp.com/oauth2/authorize?client_id=779017802396467220&scope=bot&permissions=0
 
+#id = 779017373952901122 server ID
+
 import discord
 
 # Take token from token file, just for security! :)
@@ -11,15 +13,33 @@ def read_token():
 
 token = read_token()
 
-#prueba de commit
-
+def read_server_id():
+    with open("token.txt", "r") as i:
+        lines = i.readlines()
+        return lines[1].strip()
 
 client = discord.Client()
 
 @client.event
+async def on_member_join(member):
+    for channel in member.server.channels:
+        if str(channel) == "general":
+            await client.send_message(f"""Welcome to the server {member.mention}""")
+
+
+@client.event
 async def on_message(message):
-    if message.content.find("!hi") == 0:
-        await message.channel.send("Helloooo")
+    id = client.get_guild(int(read_server_id()))
+    channels = ["commands"]
+    valid_users = ["www.josemalagon.es#2954"]
+
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hi") == 0:
+            await message.channel.send("Helloooo")
+        elif message.content == "!users":
+            await  message.channel.send(f"""# of Members {id.member_count}""")
+
+
 
 
 client.run(token)
